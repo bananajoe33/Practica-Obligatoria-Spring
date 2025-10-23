4eaccfae3e72a811304e0dc52f83808bc39e7995
bananajoe33 <adriddfc@gmail.com>
2025-10-23 12:28:49 -0500

    creacion del proyecto

---
+++ src/test/java/es/etg/daw/dawes/java/web/practica/instituto/InstitutoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.etg.daw.dawes.java.web.practica.instituto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
